93706d0dd62e1625a0197031d370976ef98c9330
moa3337 <moa-x@hotmail.com>
2023-02-06 14:36:14 +0100

    setting cod

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+ Utilizzando i dati forniti, creare un array di oggetti
+ per rappresentare i membri del team. Ogni membro
+ è caratterizzato dalle seguenti informazioni: nome,
+ ruolo e foto.
+ Creare l’array di oggetti con le informazioni fornite.
+ Stampare su console, per ogni membro del team, le
+ informazioni di nome, ruolo e la stringa della foto.
+ Stampare le stesse informazioni su DOM sottoforma di stringhe.
+*/
