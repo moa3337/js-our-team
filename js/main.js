069721a094f17e55dc1e3bf71d72f330626f4f7d
moa3337 <moa-x@hotmail.com>
2023-02-07 18:34:55 +0100

    correzione final commit

--- js/main.js
+++ js/main.js
@@ -12,62 +12,68 @@
 // DIACHIARAZIONE DELL'ARRAY DEL NOME DEL TEAM
 const teamMembers = [{
     // DESCRIZIONE DEI MEMBRI DEL TEAM (CON "object")
-    photo: "pictures/wayne-barnett-founder-ceo.jpg",
+    photo: "wayne-barnett-founder-ceo.jpg",
     memberName: "Wayne Barnett",
     role: "Founder & CEO",
 },
 {
-    photo: "pictures/angela-caroll-chief-editor.jpg",
+    photo: "angela-caroll-chief-editor.jpg",
     memberName: "Angela Caroll",
     role: "Chief Editor",
 },
 {
-    photo: "pictures/walter-gordon-office-manager.jpg",
+    photo: "walter-gordon-office-manager.jpg",
     memberName: "Walter Gordon",
     role: "Office Manager",
 },
 {
-    photo: "pictures/angela-lopez-social-media-manager.jpg",
+    photo: "angela-lopez-social-media-manager.jpg",
     memberName: "Angela Lopez",
     role: "Social Media Manager",
 },
 {
-    photo: "pictures/scott-estrada-developer.jpg",
+    photo: "scott-estrada-developer.jpg",
     memberName: "Scott Estrada",
     role: "Developer",
 },
 {
-    photo: "pictures/barbara-ramos-graphic-designer.jpg",
+    photo: "barbara-ramos-graphic-designer.jpg",
     memberName: "Barbara Ramos",
     role: "Graphic Designer",
 }];
 
-// FUNZIONE PER OGNI MEMBRO DEL TEAM
-teamMembers.forEach(function (member) {
+// AGGANCIO AL "container" TRAMITE "id" DEL HTML 
+const containerEl = document.getElementById("container");
+
+// FUNZIONE PER STAMPARE OGNI MEMBRO DEL TEAM
+for (const member of teamMembers) {
+    console.log("name: " + member.memberName);
+    console.log("role: " + member.role);
+    console.log("photo: " + member.photo);
+
+    // MANIPOLAZIONE DEL DOM PER OGNI MEMBRO DEL TEAM
+    const memberInfo = `
+    <div class="col">  
+    <div class="card">
+    <img src="./pictures/${member.photo}" class="card-img-top" alt="${member.memberName}">
+    <div class="card-body">
+    <h5 class="card-title">${member.memberName}</h5>
+    <p class="card-text">${member.role}</p>
+    </div>
+    </div>
+    </div>    
+    `;
+
+    containerEl.innerHTML += memberInfo;
+};
+
+
+// FUNZIONE ALTERNATIVA PER STAMPARE OGNI MEMBRO DEL TEAM
+/*teamMembers.forEach(function (member) {
     console.log(`
     memberName: ${member.memberName}, 
     role: ${member.role}, 
     photo: ${member.photo}
     `);
-})
-
-// AGGANCIO ALL'"id" DEL HTML 
-const container = document.getElementById("container");
-
-// MANIPOLAZIONE DEL DOM PER OGNI MEMBRO DEL TEAM
-teamMembers.forEach(function (member) {
-    const memberInfo = `
-        <div class="col">  
-            <div class="card">
-                <img src="${member.photo}" class="card-img-top" alt="${member.memberName}">
-                <div class="card-body">
-                    <h5 class="card-title">${member.memberName}</h5>
-                    <p class="card-text">${member.role}</p>
-                </div>
-            </div>
-        </div>    
-    `;
-
-    container.innerHTML += memberInfo;
-});
+})*/
 
